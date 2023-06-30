525781c0a2efab6cf36458a03379e322b8f661d6
Aziz Chikh <azizchpay99@outlook.com>
2023-06-30 16:03:06 +0200

    Add Wine dataset and *dataloaders.py file

---
+++ pytorch_dataloaders.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import pandas as pd
+import math
+
+
+class WineDataset(Dataset):
+
+    def __init__(self):
+        #load data
+
+    def __getitem__(self, index):
+        pass
+
+    
+    def __len__():
+        pass
+
